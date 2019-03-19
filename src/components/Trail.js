ac5a02446188bed0a9de1f864f68a52d78c9f327
grantfogle <grantjfogle@gmail.com>
2019-03-19 17:40:34 -0600

    styling trail view

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Trail = ({ name, date }) => {
+const Trail = ({ trail }) => {
+    const { location, name, region, status } = trail;
     return (
         <div className="trail">
             {/*id*/}
             {/*icon*/}
+            <div className="trail-name">{name}</div>
             {/*name*/}
+            <div className="trail-region">{region}</div>
             {/*region*/}
+            <div className="trail-location">{location}</div>
             {/*location*/}
+            <div className="trail-status">{status}</div>
             {/*status*/}
             {/*reason for closure*/}
             {/*weather - five day forecast*/}
