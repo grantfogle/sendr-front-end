ffc4fa90e80b29330054ed137fc7c9641d74a6d9
grantfogle <grantjfogle@gmail.com>
2019-03-19 16:32:48 -0600

    adding filter toggle function and styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import Filter from './components/Filter';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      filter: '',
+      clicked: false
+    }
+  }
+
+  clickedFilter = () => {
+    return this.setState({ clicked: !this.state.clicked });
+  }
+
   render() {
     return (
       <div className="App">
-        <Filter />
+        <Filter filter={this.state.filter} clicked={this.state.clicked} clickedFilter={this.clickedFilter} />
       </div>
     );
   }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,10 +4,24 @@ import Dropdown from 'react-bootstrap/Dropdown';
 //get some icons up in here
 import '../App.css'
 
-const Filter = () => {
+const filterButtons = () => {
+    return (
+        <ul className="filter-list">
+            <li>Front Range</li>
+            <li>Salida/Canyon City</li>
+            <li>Crested Butte/Gunnison</li>
+            <li>Park County</li>
+            <li>Grand Junction/Fruita</li>
+            <li>Colorado Springs</li>
+        </ul>
+    )
+}
+
+const Filter = ({ filter, clicked, clickedFilter }) => {
     return (
         <div className="main">
-            <button className="filter">Where are you riding</button>
+            <button className="filter" onClick={() => clickedFilter()}> Where are you riding?</button>
+            {clicked ? filterButtons() : ''}
             {/* <DropdownButton id="dropdown-basic-button" className="filter" title="Dropdown button">
                 <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
